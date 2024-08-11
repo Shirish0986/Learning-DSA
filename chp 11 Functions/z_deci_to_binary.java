020fe48f8d6f9ee6b24393c2ea044a6f78f414ad
Shirish0986 <digitalcrunch.z@gmail.com>
2024-08-11 22:13:27 +0530

    Updating code BinToDecimal

---
+++ chp 11 Functions/z_deci_to_binary.java
@@ -0,0 +1,15 @@
+public class z_deci_to_binary {
+
+    public static void deciTobin(int n){
+        while (n>0) {
+            
+        }
+      
+
+    }
+    public static void main(String[] args) {
+        int num=9;
+        deciTobin(num);
+        
+    }
+}
