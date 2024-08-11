a6844548a45c49ca907ae98c626e36c14ad5bd7e
Shirish0986 <digitalcrunch.z@gmail.com>
2024-08-11 22:25:22 +0530

    Uploading code DecimalToBinary

--- chp 11 Functions/z_deci_to_binary.java
+++ chp 11 Functions/z_deci_to_binary.java
@@ -1,15 +1,21 @@
 public class z_deci_to_binary {
 
-    public static void deciTobin(int n){
-        while (n>0) {
-            
+    public static void deciTobin(int n) {
+        int binnum = 0;
+        int pow = 0;
+        while (n > 0) {
+            int rem = n % 2;
+            binnum = binnum + (rem * (int) Math.pow(10, pow));
+            pow++;
+            n = n / 2;
         }
-      
+        System.out.println(binnum);
 
     }
+
     public static void main(String[] args) {
-        int num=9;
-        deciTobin(num);
-        
+
+        deciTobin(9);
+
     }
 }
