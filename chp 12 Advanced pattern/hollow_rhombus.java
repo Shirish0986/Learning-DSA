59f3655649ff21547d4c04f5589c54fb16f16886
Shirish0986 <digitalcrunch.z@gmail.com>
2024-08-16 18:35:15 +0530

    Solved Hollow_Rhombus

--- chp 12 Advanced pattern/hollow_rhombus.java
+++ chp 12 Advanced pattern/hollow_rhombus.java
@@ -1,25 +1,25 @@
-public class hollow_rhombus{
+public class hollow_rhombus {
 
     public static void hollowrhombus(int n) {
         for (int i = 1; i <= n; i++) {
 
-            //spaces = (n-i)
-            for (int j = 1; j <=n-i; j++) {
+            // spaces = (n-i)
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
             // star = n (hollow rectangle)
             for (int j = 1; j <= n; j++) {
-                if (i==1 || i==n || j==1 ||j==n) {     //for boundary 
+                if (i == 1 || i == n || j == 1 || j == n) { // for boundary
                     System.out.print("*");
-                }
-                else{
+                } else {
                     System.out.print(" ");
                 }
             }
             System.out.println();
         }
     }
+
     public static void main(String[] args) {
         hollowrhombus(5);
     }
