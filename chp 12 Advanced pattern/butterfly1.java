e5ebba8e06870a757507f3a863adef9c237ade65
Shirish0986 <digitalcrunch.z@gmail.com>
2024-08-16 18:25:37 +0530

    Solved Butterfly_Patters

--- chp 12 Advanced pattern/butterfly1.java
+++ chp 12 Advanced pattern/butterfly1.java
@@ -16,6 +16,22 @@ public class butterfly1 {
             System.out.println();
         }
 
+        for (int i = n; i >= 1; i--) {
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 
     public static void main(String[] args) {
