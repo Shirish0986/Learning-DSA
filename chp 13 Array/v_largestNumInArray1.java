e4017705e7da7472289eeb25b18f57c6b992e7c3
Shirish0986 <digitalcrunch.z@gmail.com>
2024-08-22 23:12:02 +0530

    Revising Arrays

--- chp 13 Array/v_largestNumInArray1.java
+++ chp 13 Array/v_largestNumInArray1.java
@@ -1,13 +1,16 @@
 public class v_largestNumInArray1 {
     public static int largestInterger(int num[]){
+        int largestnum=Integer.MIN_VALUE;
         for (int i = 1; i < num.length; i++) {
-            if (condition) {
-                
+            if (num[i]>largestnum) {
+                largestnum=num[i];
             }
+            
         }
+        return largestnum;
     }
     public static void main(String[] args) {
-        int num[]={2,4,6,8,10,12};
+        int num[]={2,4,6,81,10,12};
         int largest=largestInterger(num);
         System.out.println(largest);
 
