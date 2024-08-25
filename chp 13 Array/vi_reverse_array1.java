7908400f8f191f10c9196eb509a680c5842c5a92
Shirish0986 <digitalcrunch.z@gmail.com>
2024-08-25 22:15:28 +0530

    Revising Arrays

--- chp 13 Array/vi_reverse_array1.java
+++ chp 13 Array/vi_reverse_array1.java
@@ -1,5 +1,23 @@
 public class vi_reverse_array1 {
+    public static void reversearray(int num[]) {
+        int start = 0, end = num.length - 1;
+        while (start < end) {
+            int temp = num[start];
+            num[start] = num[end];
+            num[end] = temp;
+            start++;
+            end--;
+        }
+
+    }
+
     public static void main(String[] args) {
-        
+        int num[] = { 2, 4, 6, 8, 10 };
+        reversearray(num);
+        // printing the reverse array
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i] + " ");
+        }
+
     }
 }
